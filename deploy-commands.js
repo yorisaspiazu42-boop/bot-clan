b92ebdb1e7e91e988a0980a64a16b1272b229be0
yorisaspiazu42 <yorisaspiazu42@gmail.com>
2026-06-14 03:55:28 -0500

    Cambiados comandos a globales

--- deploy-commands.js
+++ deploy-commands.js
@@ -25,10 +25,9 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
         console.log("🔄 Registrando comandos...");
 
         await rest.put(
-            Routes.applicationGuildCommands(
-                process.env.CLIENT_ID,
-                process.env.GUILD_ID
-            ),
+    Routes.applicationCommands(
+        process.env.CLIENT_ID
+    ),
             {
                 body: commands
             }
